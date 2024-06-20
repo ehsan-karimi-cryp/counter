1a8f7230acc701d502a0862e6d7fc5f4c93223b8
Ehsan Karimi <ehsan.karimi.cryp@gmail.com>
2024-06-20 10:35:32 +0330

    buttons transform scale on click added

--- js/script.js
+++ js/script.js
@@ -17,12 +17,24 @@ function textCounter(x) {
 buttons[0].addEventListener("click", () => {
   counterValue = counterValue + 1;
   counter.innerHTML = textCounter(counterValue);
+  buttons[0].style.transform = "scale(1.5)";
+  setTimeout(() => {
+    buttons[0].style.transform = "scale(1)";
+  }, 100);
 });
 buttons[1].addEventListener("click", () => {
   counterValue = 0;
   counter.innerHTML = textCounter(counterValue);
+  buttons[1].style.transform = "scale(1.5)";
+  setTimeout(() => {
+    buttons[1].style.transform = "scale(1)";
+  }, 100);
 });
 buttons[2].addEventListener("click", () => {
   counterValue = counterValue - 1;
   counter.innerHTML = textCounter(counterValue);
+  buttons[2].style.transform = "scale(1.5)";
+  setTimeout(() => {
+    buttons[2].style.transform = "scale(1)";
+  }, 100);
 });
